d277768a4766c174ec0093a38ec8ca901aa101dc
kkdd <kkondo_z0@nifty.com>
2023-12-08 23:24:01 +0900

    Update andoyerlambert.js

--- andoyerlambert.js
+++ andoyerlambert.js
@@ -1,19 +1,58 @@
 function andoyerlambert(lat1, lon1, lat2, lon2){
   "use strict";
-  const a = 6378137.0;
-  const f = 1 / 298.257223563;
+  const a = 6378137.0;  // GRS80
+  const f = 1 / 298.257223563;  // WGS84
+  const d_antipodal = 0.9983242984312529 * Math.PI;  // half meridian arc length
   const degree = Math.PI / 180.0;
-  const rlat1 = lat1 * degree;
-  const rlon1 = lon1 * degree;
-  const rlat2 = lat2 * degree;
-  const rlon2 = lon2 * degree;
+  const sin = Math.sin;
+  const cos = Math.cos;
+  const tan = Math.tan;
+  const asin = Math.asin;
+  const atan = Math.atan;
+  const sqrt = Math.sqrt;
+  const hypot = Math.hypot;
   
-  const p1 = Math.atan((1 - f) * Math.tan(rlat1));
-  const p2 = Math.atan((1 - f) * Math.tan(rlat2));
-  const sd = Math.acos(Math.sin(p1) * Math.sin(p2) + Math.cos(p1) * Math.cos(p2) * Math.cos(rlon2 - rlon1));
-  const c = (Math.sin(sd) - sd) * ((Math.sin(p1) + Math.sin(p2)) / Math.cos(0.5 * sd)) ** 2;
-  const s = (Math.sin(sd) + sd) * ((Math.sin(p1) - Math.sin(p2)) / Math.sin(0.5 * sd)) ** 2;
-  const delta = 0.125 * f * (c - s);
-  
-  return a * (sd + delta);
+  const theta1 = atan((1 - f) * tan(lat1 * degree));  // parametric (reduced) latitude
+  const theta2 = atan((1 - f) * tan(lat2 * degree));
+  const theta = (theta1 + theta2) / 2; // middle
+  const thetadiffhalf = (theta1 - theta2) / 2;
+  const londiffhalf = (lon1 - lon2) / 2 * degree;
+
+  const cos_theta = cos(theta);
+  const sin_thetadiffhalf = sin(thetadiffhalf);
+  const sin_d_half = hypot(  // sin(d/2) = chord_half
+    sin(londiffhalf) * cos_theta,
+    cos(londiffhalf) * sin_thetadiffhalf
+  );
+  if (sin_d_half <= 0) {return 0}
+  if (sin_d_half >= 1) {return a * d_antipodal}
+
+  const d = 2 * asin(sin_d_half);  // 0-th order distance; arc_half = asin(chord_half)
+
+  const cos_d_half = sqrt(1 - sin_d_half ** 2);
+  const sin_d = 2 * sin_d_half * cos_d_half;
+  const sin_theta = sqrt(1 - cos_theta ** 2);
+  const cos_thetadiffhalf = sqrt(1 - sin_thetadiffhalf ** 2);
+  const c = (sin_d - d) * (sin_theta * cos_thetadiffhalf / cos_d_half) ** 2;
+  const s = (sin_d + d) * (cos_theta * sin_thetadiffhalf / sin_d_half) ** 2;
+  const delta_1st = f * (c - s) / 2;  // 1st order
+  if (Math.abs(delta_1st) > 0.01) {return a * d}
+
+  return a * (d + delta_1st);
+}
+
+function andoyerlambert_using_cosine_law(lat1, lon1, lat2, lon2){
+  "use strict";
+  const a = 6378137.0;  // GRS80
+  const f = 1 / 298.257223563;  // WGS84
+  const degree = Math.PI / 180.0;  
+  const londiff = (lon1 - lon2) * degree;
+  const theta1 = Math.atan((1 - f) * Math.tan(lat1 * degree));  // parametric (reduced) latitude
+  const theta2 = Math.atan((1 - f) * Math.tan(lat2 * degree));
+  const d = Math.acos(Math.sin(theta1) * Math.sin(theta2) + Math.cos(theta1) * Math.cos(theta2) * Math.cos(londiff));
+  const c = (Math.sin(d) - d) * ((Math.sin(theta1) + Math.sin(theta2)) / Math.cos(d / 2)) ** 2;
+  const s = (Math.sin(d) + d) * ((Math.sin(theta1) - Math.sin(theta2)) / Math.sin(d / 2)) ** 2;
+  const delta = f * (c - s) / 8;
+  if (Math.abs(delta) > 0.01) {return a * d}
+  return a * (d + delta);
 }
