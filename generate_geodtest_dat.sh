1722f9b9d26944f8a217ce6908388ca201353bf5
kkdd <kkondo_z0@nifty.com>
2023-12-08 14:29:27 +0900

    Create generate_geodtest_dat.sh
    using GeodSolve (GeographicLib Utilities)

---
+++ generate_geodtest_dat.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# using GeodSolve (GeographicLib Utilities)
+step=1  # in degree
+dist_max=19970000  # in meter
+
+for dist_exponent in $(seq 0 7); do
+for dist_significand in 1 3; do
+  dist=$((10 ** dist_exponent * dist_significand))
+  [ $dist -gt $dist_max ] && dist=$dist_max
+  for dir in $(seq 0 $step 90); do
+    for lat in $(seq -90 $step 90); do
+      echo $lat 0 $dir $dist | sh -c 'w="$(cat)"; /bin/echo -n "$w "; GeodSolve -p 9 --input-string "$w"' | awk '{print $1,$2,$3,$5,$6,$7,$4}'
+    done
+  done
+done
+done
